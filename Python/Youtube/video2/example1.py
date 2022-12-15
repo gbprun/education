4fffee9900e3dc429c58b4afc48c85d2a51acf2a
Igor Povalyaev <gbprun@gmail.com>
2022-12-15 10:48:02 +0300

    Youtube. Python. Lesson2

---
+++ Python/Youtube/video2/example1.py
@@ -0,0 +1,11 @@
+class Point:
+    color = 'red'
+    circle = 2
+
+    def set_coords():
+        print("Вызов метода set_coords")
+    
+
+# вызвов метода класса
+Point.set_coords()
+print(Point.set_coords)
